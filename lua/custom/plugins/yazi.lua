2576812050b3390eb8e00473d459fbf6807a406c
Maksym Mylokostenko <m.mylokostenko@gmail.com>
2026-04-19 00:50:34 +0300

    feat(plugins): add and configure custom plugins yazi, hardtime, and avante
    - Add `yazi.nvim` to replace netrw with advanced floating window options.
    - Add `hardtime.nvim` to enforce better Neovim movement habits.
    - Add `avante.nvim` plugin specification (currently disabled).
    - Enable Nerd Font rendering in `init.lua`.
    - Set global statusline (`laststatus = 3`) and apply stylua formatting.

---
+++ lua/custom/plugins/yazi.lua
@@ -0,0 +1,54 @@
+return {
+  'mikavilpas/yazi.nvim',
+  version = '*', -- use the latest stable version
+  event = 'VeryLazy',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', lazy = true },
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      '<leader>-',
+      mode = { 'n', 'v' },
+      '<cmd>Yazi<cr>',
+      desc = 'Open yazi at the current file',
+    },
+    {
+      -- Open in the current working directory
+      '<leader>cw',
+      '<cmd>Yazi cwd<cr>',
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      '<c-up>',
+      '<cmd>Yazi toggle<cr>',
+      desc = 'Resume the last yazi session',
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = true,
+
+    -- Make the floating window slightly larger for better visibility
+    floating_window_scaling_factor = 0.9,
+
+    -- Add a nice rounded border to the Yazi window
+    yazi_floating_window_border = 'rounded',
+
+    -- Highlight open buffers in the same directory as the hovered file
+    highlight_hovered_buffers_in_same_directory = true,
+
+    keymaps = {
+      show_help = '<f1>',
+      -- You can also override default internal keymaps here if desired
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- mark netrw as loaded so it's not loaded at all.
+    --
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
--- lua/options.lua
+++ lua/options.lua
@@ -19,7 +19,9 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -68,5 +70,7 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.o.laststatus = 3
 
 -- vim: ts=2 sts=2 sw=2 et
