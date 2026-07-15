90dd852f2618b897a927ada0b4f80cb989eb8de6
Maksym Mylokostenko <m.mylokostenko@gmail.com>
2026-07-16 00:22:25 +0300

    fix(snacks): remove lazy dependency from dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -8,7 +8,13 @@ vim.print = _G.dd -- Override print to use snacks for `:=` command
 
 require('snacks').setup {
   bigfile = { enabled = true },
-  dashboard = { enabled = true },
+  dashboard = {
+    enabled = true,
+    sections = {
+      { section = 'header' },
+      { section = 'keys', gap = 1, padding = 1 },
+    },
+  },
   indent = { enabled = true },
   input = { enabled = true },
   notifier = {
